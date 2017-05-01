a727be19de04cfd5d69260d18f6710ccb061c76c
Thomas Kinsella <tkspam2002@yahoo.com>
2017-05-01 14:25:44 +0100

    Initial commit, Completed Build

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static  GameManager instance;
+	public bool gameOver;
+
+	void Awake()
+	{
+		if (instance == null) 
+		{
+			instance = this;
+		}
+	}
+
+	void Start () 
+	{
+		gameOver = false;
+	}
+
+	void Update () 
+	{
+		
+	}
+
+	public void StartGame()
+	{
+		UIManager.instance.GameStart ();
+		//ScoreManager.instance.startScore ();
+		GameObject.Find ("PlatformSpawner").GetComponent<PlatformSpawner> ().StartSpawningPlatforms ();
+	}
+
+	public void GameOver()
+	{
+		UIManager.instance.GameOver ();
+		ScoreManager.instance.StopScore ();
+		gameOver = true;
+	}
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+	public static UIManager instance;
+
+	public GameObject TopPanel;
+	public GameObject gameOverPanel;
+	public GameObject taptext;
+	public Text score;
+	public Text highScore1;
+	public Text highScore2;
+
+	void Awake()
+	{
+		if (instance == null) 
+		{
+			instance = this;
+		}
+	}
+
+	void Start () 
+	{
+		highScore1.text = "High Score: " + PlayerPrefs.GetInt ("highScore");
+	}
+	
+	public void GameStart()
+	{
+		taptext.SetActive (false);
+		TopPanel.GetComponent<Animator> ().Play ("PanelUp");
+	}
+
+	public void GameOver ()
+	{
+		score.text = PlayerPrefs.GetInt ("score").ToString();
+		highScore2.text = PlayerPrefs.GetInt ("highScore").ToString();
+		gameOverPanel.SetActive (true);
+	}
+
+	public void Reset()
+	{
+		SceneManager.LoadScene ("Level1");
+	}
+
+	void Update () {
+		
+	}
+}
